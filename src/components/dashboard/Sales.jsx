0f981643460e6d164fa19848c7dc03ba60cb7d7e
pwilson77 <p.o.yirenkyi@gmail.com>
2021-05-08 20:51:41 +0000

    Added orders table

--- src/components/dashboard/Sales.jsx
+++ src/components/dashboard/Sales.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
 export default function Sales(props) {
+  const server_url = "https://eos-adinkrah-enterprise-api.herokuapp.com/";
+  const [orders, setOrders] = useState([]);
+  const [isFetched, setIsFetched] = useState(false);
+
+  useEffect(() => {
+    if (!isFetched) {
+      fetchUserOrders();
+      setIsFetched(true);
+    }
+  });
+
+  const fetchUserOrders = () => {
+    let user_details = JSON.parse(localStorage.getItem("user_details"));
+    axios
+      .get(`${server_url}orders/${user_details.id}`)
+      .then((res) => setOrders(res.data.message))
+      .catch((e) => alert("Error with fetching"));
+  };
+
   return (
     <React.Fragment>
       <div class="main-panel">
@@ -9,128 +29,29 @@ export default function Sales(props) {
         <div class="content">
           <div class="container-fluid">
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-6">
                 <div class="card">
                   <div class="header">
-                    <h4 class="title">Striped Table with Hover</h4>
-                    <p class="category">Here is a subtitle for this table</p>
+                    <h4 class="title">Your Orders</h4>
+                    <p class="category">Orders</p>
                   </div>
                   <div class="content table-responsive table-full-width">
                     <table class="table table-hover table-striped">
                       <thead>
                         <th>ID</th>
-                        <th>Name</th>
-                        <th>Salary</th>
-                        <th>Country</th>
-                        <th>City</th>
+                        <th>Product Name</th>
+                        <th>Total</th>
+                        <th>Date</th>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td>1</td>
-                          <td>Dakota Rice</td>
-                          <td>$36,738</td>
-                          <td>Niger</td>
-                          <td>Oud-Turnhout</td>
-                        </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Minerva Hooper</td>
-                          <td>$23,789</td>
-                          <td>Curaçao</td>
-                          <td>Sinaai-Waas</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>Sage Rodriguez</td>
-                          <td>$56,142</td>
-                          <td>Netherlands</td>
-                          <td>Baileux</td>
-                        </tr>
-                        <tr>
-                          <td>4</td>
-                          <td>Philip Chaney</td>
-                          <td>$38,735</td>
-                          <td>Korea, South</td>
-                          <td>Overland Park</td>
-                        </tr>
-                        <tr>
-                          <td>5</td>
-                          <td>Doris Greene</td>
-                          <td>$63,542</td>
-                          <td>Malawi</td>
-                          <td>Feldkirchen in Kärnten</td>
-                        </tr>
-                        <tr>
-                          <td>6</td>
-                          <td>Mason Porter</td>
-                          <td>$78,615</td>
-                          <td>Chile</td>
-                          <td>Gloucester</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-12">
-                <div class="card card-plain">
-                  <div class="header">
-                    <h4 class="title">Table on Plain Background</h4>
-                    <p class="category">Here is a subtitle for this table</p>
-                  </div>
-                  <div class="content table-responsive table-full-width">
-                    <table class="table table-hover">
-                      <thead>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Salary</th>
-                        <th>Country</th>
-                        <th>City</th>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>1</td>
-                          <td>Dakota Rice</td>
-                          <td>$36,738</td>
-                          <td>Niger</td>
-                          <td>Oud-Turnhout</td>
-                        </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Minerva Hooper</td>
-                          <td>$23,789</td>
-                          <td>Curaçao</td>
-                          <td>Sinaai-Waas</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>Sage Rodriguez</td>
-                          <td>$56,142</td>
-                          <td>Netherlands</td>
-                          <td>Baileux</td>
-                        </tr>
-                        <tr>
-                          <td>4</td>
-                          <td>Philip Chaney</td>
-                          <td>$38,735</td>
-                          <td>Korea, South</td>
-                          <td>Overland Park</td>
-                        </tr>
-                        <tr>
-                          <td>5</td>
-                          <td>Doris Greene</td>
-                          <td>$63,542</td>
-                          <td>Malawi</td>
-                          <td>Feldkirchen in Kärnten</td>
-                        </tr>
-                        <tr>
-                          <td>6</td>
-                          <td>Mason Porter</td>
-                          <td>$78,615</td>
-                          <td>Chile</td>
-                          <td>Gloucester</td>
-                        </tr>
+                        {orders.map((order) => (
+                          <tr>
+                            <td>{order.orderID}</td>
+                            <td>{order.products}</td>
+                            <td>{order.total}</td>
+                            <td>{order.date}</td>
+                          </tr>
+                        ))}
                       </tbody>
                     </table>
                   </div>
